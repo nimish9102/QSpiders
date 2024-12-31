26f2b97989011c6f243c336c938e466e9cdd4bcd
qspiders <nimishprajapati2002@gmail.com>
2024-12-31 11:37:27 +0530

    java,web tech and database forlders are included <br> now ready to work

---
+++ Java/biodata.java
@@ -0,0 +1,26 @@
+class biodata
+{
+    public static void main(String args[])
+    {
+        String name = "Nimish Ghatliya";
+        String dob = "09/10/2002";
+        String caste = "Prajapati";
+        long mno = 96385207410L;
+        char zender = 'm';
+        int age = 22;
+        
+	float per= 83.66f;
+	double salary = 2000000.255d;
+
+        System.out.println("Name is "+name);
+        System.out.println("Birth date is "+dob);
+        System.out.println("Caste is "+caste);
+        System.out.println("Mobile number is "+mno);
+        System.out.println("Zender is "+zender);
+	System.out.println("Percentage is "+per);
+	System.out.println("Salary is "+salary);
+
+
+
+    }
+}
